1536bdaff6fa6f47a580506420865db9267e303b
theumairaliofficial <umairali.official90@gmail.com>
2026-06-16 17:20:33 +0500

    Day 1: variables, operators, strings

---
+++ js-day1-variables-operators-strings.js
@@ -0,0 +1,23 @@
+// Variables
+const myName = "Umair Ali";
+const university = "Arid Agriculture University";
+let goal = "Top 1% Software Engineer";
+
+// Template Literal
+console.log(`My name is ${myName}. I study at ${university}. My goal is to become a ${goal}.`);
+
+// Operators
+let price = 3500;
+let discount = 10;
+let finalPrice = price - (price * discount / 100);
+console.log(`Original Price: ${price}`);
+console.log(`Final Price after ${discount}% discount: ${finalPrice}`);
+
+// Comparison
+console.log(price > finalPrice);
+console.log(finalPrice === 3150);
+
+// Array
+const skills = ["HTML", "CSS", "JavaScript"];
+skills.push("React");
+console.log(skills);
